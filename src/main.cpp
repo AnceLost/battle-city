54b45ac2fdcaa5e9cda26ee5b8bea34a278c9d27
AnceLost <jayseven228@yandex.ru>
2024-08-07 20:36:06 +0300

    first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,39 @@
 
 #include <iostream>
 
+GLfloat points[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
+
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+"   frag_color = vec4(color, 1.0);"
+"}";
+
 
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height) {
     g_windowSizeX = width;
@@ -62,13 +93,62 @@ int main(void)
     std::cout << "OpenGL version " << glGetString(GL_VERSION) << std::endl;
     std::cout << "OpenGl " << GLVersion.major << "," <<  GLVersion.minor << std::endl;
 
-    glClearColor(0, 1, 0, 1);
+    glClearColor(1, 1, 0, 1);
+
+    //Создаём два шейдера - vertex и fragment
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    //Создаём программу и привязываем к ней шейдеры
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program);
+
+    //Шейдеры уже привязаны, удаляем
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    //Передаем видеокарте данные через буферы
+    GLuint points_vbo = 0; //id буфера, через него обращаемся
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    // BufferData работает с текущим привязанным буфером (строчка выше)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    //Ещё нужен vertexArrayObject (vao)
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    //Подключаем данные из буферов в шейдеры
+    glEnableVertexAttribArray(0); //Включаем location "0", который мы указывали в шейдере
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo); //делаем этот буфер текущим
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pwindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pwindow);
